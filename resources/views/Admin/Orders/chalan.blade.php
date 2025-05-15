33d9b268f4c9e3d44e5406daedcb1c70285d624e
ma_abdullah <maabdullah511@gmail.com>
2025-05-16 01:03:26 +0600

    invoice and chalan design

--- resources/views/Admin/Orders/chalan.blade.php
+++ resources/views/Admin/Orders/chalan.blade.php
@@ -1,154 +1,219 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 @php
-    $data=\App\Models\Order::where('invoice_no',$invoice_no)->first();
-    $orders=\App\Models\OrderDetails::where('order_id',$data->id)->get();
+    $data = \App\Models\Order::where('invoice_no', $invoice_no)->first();
+    $orders = \App\Models\OrderDetails::where('order_id', $data->id)->get();
 @endphp
+
 <head>
-    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+    <meta charset="UTF-8">
     <title>Chalan {{ $data->callan_no }}</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
-    <meta name="msapplication-TileColor" content="#ffffff">
-
-    <meta name="theme-color" content="#ffffff">
     <style>
-
-        * { margin: 0; padding: 0; }
         body {
-            font: 14px/1.4 Helvetica, Arial, sans-serif; background: url("{{ asset('public/images') }}/pad.png"); background-size:cover;
+            font: 14px/1.4 Helvetica, Arial, sans-serif;
+            background-size: cover;
+            margin: 0;
+            padding: 0 0 100px 0;
         }
-        #page-wrap { width: 800px; margin: 0 auto; }
 
-        textarea { border: 0; font: 14px Helvetica, Arial, sans-serif; overflow: hidden; resize: none; }
-        table { border-collapse: collapse; }
-        table td, table th { border: 1px solid black; padding: 5px; }
+        #page-wrap {
+            width: 800px;
+            margin: 0 auto;
+            position: relative;
+        }
 
-        #header { height: 15px; width: 100%; margin: 20px 0; background: #222; text-align: center; color: white; font: bold 15px Helvetica, Sans-Serif; text-decoration: uppercase; letter-spacing: 20px; padding: 8px 0px; }
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
 
-        #address { width: 250px; height: 150px; float: left; }
-        #customer { overflow: hidden; }
+        table td,
+        table th {
+            border: 1px solid black;
+            padding: 5px;
+        }
 
-        #logo { text-align: right; float: right; position: relative; margin-top: 25px; border: 1px solid #fff; max-width: 540px; overflow: hidden; }
-        #customer-title { font-size: 20px; font-weight: bold; float: left; }
+        #header {
+            background: #222;
+            color: white;
+            text-align: center;
+            font-weight: bold;
+            padding: 10px;
+        }
 
-        #meta { margin-top: 1px; width: 100%; float: right; }
-        #meta td { text-align: right;  }
-        #meta td.meta-head { text-align: left; background: #eee; }
-        #meta td textarea { width: 100%; height: 20px; text-align: right; }
+        #company-info {
+            /* margin-bottom: 05px; */
+        }
 
-        #items { clear: both; width: 100%; margin: 30px 0 0 0; border: 1px solid black; }
-        #items th { background: #eee; }
-        #items textarea { width: 80px; height: 50px; }
-        #items tr.item-row td {  vertical-align: top; }
-        #items td.description { width: 300px; }
-        #items td.item-name { width: 175px; }
-        #items td.description textarea, #items td.item-name textarea { width: 100%; }
-        #items td.total-line { border-right: 0; text-align: right; }
-        #items td.total-value { border-left: 0; padding: 10px; }
-        #items td.total-value textarea { height: 20px; background: none; }
-        #items td.balance { background: #eee; }
-        #items td.blank { border: 0; }
+        .company-left {
+            float: left;
+            width: 50%;
+        }
 
-        #terms { text-align: center; margin: 20px 0 0 0; }
-        #terms h5 { text-transform: uppercase; font: 13px Helvetica, Sans-Serif; letter-spacing: 10px; border-bottom: 1px solid black; padding: 0 0 8px 0; margin: 0 0 8px 0; }
-        #terms textarea { width: 100%; text-align: center;}
-        .printFooter{ width: 100%; text-align: center; color:#36F;}
+        .company-right {
+            float: right;
+            width: 50%;
+            text-align: right;
+        }
 
+        .clear {
+            clear: both;
+        }
 
-        .delete-wpr { position: relative; }
-        .delete { display: block; color: #000; text-decoration: none; position: absolute; background: #EEEEEE; font-weight: bold; padding: 0px 3px; border: 1px solid; top: -6px; left: -22px; font-family: Verdana; font-size: 12px; }
+        #customer {
+            /* margin-top: 20px; */
+        }
+
+        #meta td.meta-head {
+            background: #eee;
+            font-weight: bold;
+            text-align: right
+        }
+
+        #items {
+            margin-top: 15px;
+        }
+
+        #items th {
+            background: #eee;
+        }
+
+        #items td.total-line {
+            text-align: right;
+        }
+
+        #items td.total-value {
+            text-align: right;
+        }
+
+        #terms {
+            margin-top: 30px;
+        }
+
+        #terms h4 {
+            margin-bottom: 5px;
+        }
+
+        .product {
+            width: 50%;
+        }
+
+        #signatures {
+            margin-top: 50px;
+            text-align: right;
+        }
+
+        #signatures .signature-block {
+            display: inline-block;
+            text-align: right;
+        }
+
+        #footer {
+            position: fixed;
+            bottom: 0;
+            width: 800px;
+            text-align: center;
+            font-size: 13px;
+            color: #444;
+            padding: 10px 0;
+            /* background: #f5f5f5; */
+            /* border-top: 1px solid #ccc; */
+        }
     </style>
-
 </head>
 
 <body>
+    <div id="page-wrap">
+        <div id="company-info">
+            <div class="company-left">
+                <img src="{{ asset('public/images/1695193114logo.png') }}" alt="Company Logo" height="44">
+                <h2> Femina
+                    Lighting</h2>
+            </div>
+            <div class="company-right">
+                <p>
+                    <i class="fas fa-map-marker-alt"></i>
+                    <span>Plot#41, AB Super Marketing (2nd Floor)</span><br>
+                    <span style="margin-left: 20px;">Shop#46-47, Sector#03, Uttara, Dhaka</span><br>
 
-<div>
+                    <i class="fas fa-phone"></i>
+                    <span>09639117791</span><br>
 
-    <table width="100%">
-        <tr>
-            <td style="border: 0;  text-align: left" width="62%">
-                <span style="font-size: 40px; color: #2f4f4f"><!-- <strong>INVOICE</strong> --></span>
-            </td>
-            <td style="border: 0;  text-align: right" width="62%">
-                <div id="logo" style="font-size:18px;">
-                    <br>
-                    <br>
-                    <br>
-                    <br> <br>
+                    <i class="fas fa-envelope"></i>
+                    <span>info@jagopower.com</span><br>
 
-                    <!--<br /><b>Website:</b> www.my-softit.com </div></td>-->
-        </tr>
-
-
-
-    </table>
-
-    <hr>
-    <br>
-
-    <div style="clear:both"></div>
-
-    <div id="customer">
-
-        <table id="meta">
+                    <i class="fas fa-globe"></i>
+                    <span>www.jago.uddokta71.com</span>
+                </p>
+            </div>
+            <div class="clear"></div>
+        </div>
+        {{-- <hr> --}}
+        <div id="customer">
+            <table id="meta">
+                <tr>
+                    <td rowspan="4" style="text-align: left; border-right: 1px solid black;">
+                        <strong>Invoiced To</strong><br>
+                        <h3>{{ $data->customer->name }}</h3>
+                        {{ $data->customer->address }}<br>
+                        <b>Phone:</b> {{ $data->customer->phone }}<br>
+                        <b>Email:</b> {{ $data->customer->email }}
+                    </td>
+                    <td class="meta-head">CHALLAN #</td>
+                    <td>{{ $data->callan_no }}</td>
+                </tr>
+                <tr>
+                    <td class="meta-head">Challan Date</td>
+                    <td>{{ date('d-m-Y', strtotime($data->created_at)) }}</td>
+                </tr>
+            </table>
+        </div>
+        
+        <table id="items">
             <tr>
-                <td rowspan="5" style="border: 1px solid white; border-right: 1px solid black; text-align: left" width="62%">
-
-                    <strong>Invoiced To</strong> <br>
-                    <h3>{{ $data->customer->name }}</h3>
-                    {{--                    ATTN: Md.Omar Faruqe <br>--}}
-                    <br>
-                    {{ $data->customer->address }}<br>
-                    <b>Phone:</b> {{ $data->customer->phone }} <br>
-                    <b>Email: {{ $data->customer->email }}</b>
-                </td>
-                <td class="meta-head">CHALLAN #</td>
-                <td>{{ $data->callan_no }}</td>
+                <th width="65%">Product</th>
+                <th align="right">Qty</th>
             </tr>
-
-            <tr>
-
-                <td class="meta-head">Challan Date</td>
-                <td>
-                    {{ date("d-m-Y", strtotime($data->created_at)) }}
-                </td>
-            </tr>
+            @foreach ($orders as $order)
+                <tr class="item-row">
+                    <td class="description">
+                        {{ $order->name }}
+                    </td>
+                    <td align="right">
+                        {{ $order->qty }}
+                    </td>
+                </tr>
+            @endforeach
         </table>
-
+        <div id="terms">
+            <h4>Terms & Conditions</h4>
+            <p>
+                1. Dear Customer, Please ensure you have received all items mentioned in this invoice. If any item is
+                missing or
+                damaged, immediately return the package to your delivery agent.<br>
+                2. Once the package is accepted, we cannot process claims for missing, wrong, or defective
+                items.<br>
+                3. For any assistance, you are entitled to 12 months of service warranty from the date of
+                purchase.<br>
+                4. You can reach us at <strong>+8801705210280</strong> or email us at
+                <strong>info@feminalightings.com</strong>.<br>
+                5. <strong>No Money Back Policy.</strong><br>
+                6. Thank you for shopping with us. Have a great day!
+            </p>
+        </div>
+        <div id="signatures">
+            <div class="signature-block">
+                ___________________________<br>
+                <strong>Manager Signature</strong><br>
+            </div>
+        </div>
     </div>
 
-    <table id="items">
-
-        <tr>
-            <th width="65%">Product</th>
-            <th  align="right">Qty</th>
-        </tr>
-        @foreach($orders as $order)
-            <tr class="item-row">
-                <td class="description">
-                    {{ $order->name }}
-                </td>
-                <td align="right">
-                    {{ $order->qty }}
-                </td>
-            </tr>
-        @endforeach
-    </table>
-</div>
-<table border="0" style="width: 100%; margin-top:100px; border:none;">
-    <tr>
-        <td align="left" style="border:none;">
-            <b>For – Customer :</b><br />
-            <p>Received with thanks and all assigned task have been <br />completed successfully as per your requirements.</p>
-        </td>
-        <td align="right" style="border:none;">
-            ....................................<br />
-            <b>Manager Signature</b>
-        </td>
-    </tr>
-</table>
-
-<!-- <i style="font-size:14px; color:#666;"><strong>Address: </strong> 118/17, WEST SHEWRAPARA, MIRPUR, DHAKA-1216 <b>Email: </b> nasim@eskobd.com, tanbir_pshrd@yahoo.com</i> -->
+    <div id="footer">
+        <strong>Thank you for your business!</strong>
+    </div>
 </body>
+
 </html>
